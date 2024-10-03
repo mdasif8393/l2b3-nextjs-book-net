3ceefcf128a5f00385e51e6a21f787006fa2c1c2
mdasif8393 <md.asif.8393@gmail.com>
2024-10-03 18:27:21 +0600

    s

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import Banner from "@/components/banner/Banner";
+
+const HomePage = () => {
+  console.log("Home page");
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/(withCommonLayout)/posts/page.tsx
@@ -0,0 +1,9 @@
+const AllPostsPage = () => {
+  return (
+    <div>
+      <h1>all posts</h1>
+    </div>
+  );
+};
+
+export default AllPostsPage;
